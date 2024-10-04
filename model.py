8731adc3389ad994495aa7fc2b29c147a1f6a333
Mohannad Ehab Barakat <hannod98@yahoo.com>
2024-10-04 16:19:51 +0200

    prints

--- model.py
+++ model.py
@@ -104,7 +104,7 @@ def route(question):
         {"role": "system", "content": f'''You are a json expert. You answer all questions in json only. Use this json example as your guide for all answers {jsn}'''},
         {"role": "user", "content": question_template},
     ]
-
+    print(messages)
     outputs = pipeline(
         messages,
         max_new_tokens=256,
